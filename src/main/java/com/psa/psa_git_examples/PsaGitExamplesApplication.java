0893384f93d4d375f81518c3be5f5d744de7581a
Kasim <victusvenom1@gmail.com>
2025-03-08 15:23:28 +0530

    First Commit

---
+++ src/main/java/com/psa/psa_git_examples/PsaGitExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.psa.psa_git_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaGitExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaGitExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psa/psa_git_examples/PsaGitExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psa.psa_git_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsaGitExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
